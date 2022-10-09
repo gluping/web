e991436b501b3ea5a3b9f57623b20244b5fa2599
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2022-10-09 11:26:36 +0530

    fetched from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -16,3 +16,21 @@
 //const database = firebaseApp.firestore();
 //
 //export default database;
+
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyDehF_LqUodNd8hjKMmfnElG384zPy8YUQ",
+    authDomain: "gluping-6429f.firebaseapp.com",
+    databaseURL: "https://gluping-6429f-default-rtdb.firebaseio.com",
+    projectId: "gluping-6429f",
+    storageBucket: "gluping-6429f.appspot.com",
+    messagingSenderId: "951367534125",
+    appId: "1:951367534125:web:735a8cb5d43169cbc8daa4",
+    measurementId: "G-JR9BXLXT6Y"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const database = firebaseApp.firestore();
+
+  export default  database;
