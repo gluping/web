b8d70467313c0736a06749e0514a829244dd75c7
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2022-10-16 11:37:23 +0530

    added chat page with avathar from mui

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Avatar } from '@mui/material'
+import './chat.css';
 
-function Chat({ name, message, profilePic, timeStamp}) {
+function Chat({ name, message, profilePic, timestamp}) {
   return (
-    <div>
-      
+    <div className='chat'>
+      <Avatar className='chat__image' alt={name} src={profilePic}></Avatar>
+      <div className='chat__details'>
+        <h2>{name}</h2>
+        <p>{message}</p>
+
+      </div>
+
+      <p className='chat__timestamp'>{timestamp}</p>
     </div>
+
   )
 }
 
